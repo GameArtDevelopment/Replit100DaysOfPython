1586650aeeca1ffbb15cea7ce4c4a5840eb62bb5
Joseph Prez <111929236+GameArtDevelopment@users.noreply.github.com>
2023-02-27 21:22:50 -0500

    finished day 5, if/else

---
+++ 100DaysOfPython/Day5.py
@@ -0,0 +1,21 @@
+print("Marvel Movie Character Questions")
+print()
+print("This is a short marvel character questionnaire. I hope you enjoy.")
+print()
+skill = input("Do you like to read minds?. yes or no? ")
+print()
+print("So,", skill, ", on reading minds. Got it")
+print()
+if (skill == "yes"):
+    print("Hello, Professor X")
+else:
+    print("DC, all the way!!")
+print()
+friend = input("Is Wolverine a friend?. yes or no? ")
+if (friend == "yes"):
+    print()
+    print("Wolverine and the rest the X-Men solute you.")
+else:
+    print("He who remains, says 🤙🏽")
+print()
+print("Thank youu for trying my simple if/else coding experience.")
