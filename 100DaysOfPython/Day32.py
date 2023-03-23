23640f01fc8286686e76a9e53fa615ccaeb0fce3
Joseph Prez <111929236+GameArtDevelopment@users.noreply.github.com>
2023-03-23 16:49:21 -0400

    updated printing format

--- 100DaysOfPython/Day32.py
+++ 100DaysOfPython/Day32.py
@@ -1,12 +1,18 @@
 import random
+
 print()
 print("\033[34m============ Welcome to the Greeting Generator! ============\033[0m")
+
 greeting = ["Guten Tag", "Hello", "Privet", "Nǐ hǎo", "Ciao", "Konnichiwa",
             "Shalom", "Bonjour", "Goddag", "Namaste, Namaskar", "God dag"]
+
 choices = random.randint(0, 11)
+
 response = (f"Greeting: \033[32m{greeting[choices]}\033[0m")
+
 print()
-print(response)
+print(f"{response:^67}")
+
 print()
 print("\033[34m==============   Thank you trying my code    ===============\033[0m")
 print()
