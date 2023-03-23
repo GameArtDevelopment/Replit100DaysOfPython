cb38f0797eee6309a78d26c28e6b0b6617cfd03e
Joseph Prez <111929236+GameArtDevelopment@users.noreply.github.com>
2023-03-23 16:46:26 -0400

    Day 32 - Lists

---
+++ 100DaysOfPython/Day32.py
@@ -0,0 +1,12 @@
+import random
+print()
+print("\033[34m============ Welcome to the Greeting Generator! ============\033[0m")
+greeting = ["Guten Tag", "Hello", "Privet", "Nǐ hǎo", "Ciao", "Konnichiwa",
+            "Shalom", "Bonjour", "Goddag", "Namaste, Namaskar", "God dag"]
+choices = random.randint(0, 11)
+response = (f"Greeting: \033[32m{greeting[choices]}\033[0m")
+print()
+print(response)
+print()
+print("\033[34m==============   Thank you trying my code    ===============\033[0m")
+print()
