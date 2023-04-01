3360c277349500c1726278fdb63ef67054a99a6f
Joseph Prez <111929236+GameArtDevelopment@users.noreply.github.com>
2023-04-01 18:24:31 -0400

    Updated to Bill Splitter

--- 100DaysOfPython/Day10.py
+++ 100DaysOfPython/Day10.py
@@ -1,24 +1,27 @@
-print("\033[31m", "****Bill Calculator****", "\033[0m")
 print()
-bill = float(input("What is the bill amount?: "))
+print("\033[31m *** Bill Splitter *** \033[0m")
 print()
-percentage = float(input("What percentage of tip would you like to leave?. "))
+bill = float(input("What is the total bill? $"))
 print()
-numberOfPeople = int(input("How many people?: "))
+percentage = float(input("What percentage of tip would you like to leave? "))
 print()
-answer = bill / numberOfPeople
-answer = round(answer, 2)
+numOfPeople = int(input("How many people to split the bill? "))
+print()
+ans = bill / numOfPeople
+answer = round(ans, 2)
 tip = round(percentage * bill) / 100
 totalBill = round(bill + tip)
-indiBill = round((bill + tip) / numberOfPeople)
+indiBill = round((bill + tip) / numOfPeople)
 plus1 = round(indiBill * 2)
-print("You each owe", "\033[32m","$",answer, "\033[0m",
-      "and your total tip is", "\033[33m","$",tip, "\033[0m")
+print("You each owe", "\033[32m", "$", answer, "\033[0m",
+      "and your total tip is", "\033[32m", "$", tip, "\033[0m")
 print()
-print("Your total bill is", "\033[34m","$",totalBill, "\033[0m")
+print("Your total bill is", "\033[32m", "$", totalBill, "\033[0m")
 print()
-print("Including tip, each person's owes", "\033[35m","$",indiBill, "\033[0m")
+print("Including the tip, each person owes",
+      "\033[32m", "$", indiBill, "\033[0m")
 print()
 print("But because you brought your broke friend, you owe",
-      "\033[36m","$",plus1, "\033[0m")
+      "\033[32m", "$", plus1, "\033[0m")
 print()
+print("Thanks for using the Bill Splitter!")
