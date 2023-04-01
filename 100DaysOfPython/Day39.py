a12497118c6191101bc01822666767dd6a2b4f57
Joseph Prez <111929236+GameArtDevelopment@users.noreply.github.com>
2023-04-01 10:43:21 -0400

    fixed layout

--- 100DaysOfPython/Day39.py
+++ 100DaysOfPython/Day39.py
@@ -118,3 +118,4 @@ else:
     print("\033[35m \033[1mThank you for playing Hangman! I hope you enjoyed it! I had a lot of fun making it!\033[0m")
     print()
 print("Credits to https://gist.github.com/chrishorton/8510732aa9a80a03c829b09f12e20d9c for the ASCII art!")
+print()
