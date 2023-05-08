85d824f7bdf9ff995612a92f3d5b048b6ffaf192
Joseph Prez <111929236+GameArtDevelopment@users.noreply.github.com>
2023-05-07 22:03:35 -0400

    Day 76 - flask web server. Still working on it.

---
+++ 100DaysOfPython/Day76.py
@@ -0,0 +1,11 @@
+
+from flask import Flask
+
+app = Flask(__name__, static_url_path='/static')
+
+@app.route('/')
+def index():
+    return 'Hello from Flask'
+
+
+app.run(host='0.0.0.0', port = 81)
